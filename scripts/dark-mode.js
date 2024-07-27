32e10bda3dbb2806c1c811fb67533e762a5591e7
vievioorqa <patrycja.bobowska@student.uj.edu.pl>
2024-07-27 14:42:09 +0200

    dark mode

---
+++ scripts/dark-mode.js
@@ -0,0 +1,35 @@
+let darkMode = localStorage.getItem("darkMode"); // is updated every time someone clicks on it
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
+const lightModeToggle = document.querySelector("#light-mode-toggle");
+const logoElement = document.querySelector("#logoElement");
+
+const enableDarkMode = () => {
+    document.body.classList.add('dark-mode');
+    darkModeToggle.classList.add('hidden');
+    lightModeToggle.classList.remove('hidden');
+    localStorage.setItem('darkMode', 'enabled');
+    logoElement.src = "images/logo-light.png"; // Change logo for light mode
+};
+
+const disableDarkMode = () => {
+    document.body.classList.remove('dark-mode');
+    darkModeToggle.classList.remove('hidden');
+    lightModeToggle.classList.add('hidden');
+    localStorage.setItem('darkMode', 'disabled');
+    logoElement.src = "images/logo.png"; // Change logo for light mode
+};
+
+// Check the current mode and set the initial state
+if (darkMode === 'enabled') {
+    enableDarkMode();
+} else {
+    disableDarkMode();
+}
+
+darkModeToggle.addEventListener("click", () => {
+    enableDarkMode();
+});
+
+lightModeToggle.addEventListener("click", () => {
+    disableDarkMode();
+});
